30366f1bbaf1eb3698aed94df5da7b7b8850e17f
Kaiwen Sun <skw_kevin@126.com>
2022-04-16 16:03:05 -0700

    give enough time for event listener to sync token

--- event-listener-lambda/index.ts
+++ event-listener-lambda/index.ts
@@ -64,6 +64,9 @@ export const handler: SQSHandler = async (
     }
   } catch (error: any) {
     console.error(error.stack || JSON.stringify(error));
+  } finally {
+    // give enough time to refresh token
+    await new Promise(r => setTimeout(r, 5 * 1000));
   }
 };
 
